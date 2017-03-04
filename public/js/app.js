c42c2b4eaaae441489949d9fa1b0ce0b9e9280b1
TimFung93 <timothy.fung.04@gmail.com>
2017-03-04 10:55:20 -0500

    boiler plate code

---
+++ public/js/app.js
@@ -0,0 +1,1 @@
+console.log('hello coming from app')
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.use(express.static('public'));
+
+
+
+
+app.listen(8080, () => {
+	console.log('Server started on http://localhost:8080')
+	console.log('Press CTRL + C to kill server')
+})

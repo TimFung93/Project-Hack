1b2e768f334e10c2dd665fd9121762f235a2d1a3
TimFung93 <timothy.fung.04@gmail.com>
2017-03-05 04:48:43 -0500

    caregiver app

--- multiview-app/src/components/Home.js
+++ multiview-app/src/components/Home.js
@@ -18,7 +18,7 @@ export default class Home extends React.Component {
                     onDatePicked={(d) => {
                       console.log(d)
                     }}/>
-
+                <p> this is the caregiver app</p>
                 
 
             </div>
